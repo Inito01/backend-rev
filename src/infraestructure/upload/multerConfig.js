6c603540659caef2fe0456a038cc904e8ef66770
Inito01 <bpp060720@gmail.com>
2025-08-06 22:07:42 -0400

    feat: add multer configiration for file uploads [ - configure multer with diskStorage - add file type validation PDF, JPG, JPEG - set 10MB file size limit - add file filter for security ]

---
+++ src/infraestructure/upload/multerConfig.js
@@ -0,0 +1,40 @@
+import multer from 'multer';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const alloweMimeTypes = ['application/pdf', 'image/jpeg', 'image/jpg'];
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const uploadPath = path.resolve(__dirname, '..', '..', 'uploads');
+    cb(null, uploadPath);
+  },
+  filename: (req, file, cb) => {
+    const fileName = `${Date.now()}-${file.originalname}`;
+    cb(null, fileName);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (alloweMimeTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(
+      new Error(
+        'Tipo de archivo no permitido. Solo es posible subir PDF, JPG y JPEG'
+      ),
+      false
+    );
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+  limits: { fileSize: 10 * 1024 * 1024, files: 1 }, // 10 megabytes
+});
+
+export default upload;
