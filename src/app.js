8f35952b84fae564065bcf7e65b83964f454350c
Inito01 <bpp060720@gmail.com>
2025-08-10 22:09:45 -0400

    feat: added new origins for corsOptions

--- src/app.js
+++ src/app.js
@@ -16,10 +16,10 @@ const PORT = process.env.PORT || 8000;
 const corsOptions = {
   origin:
     process.env.NODE_ENV === 'production'
-      ? process.env.FRONTEND_URL
-      : ['http://localhost:5173'],
+      ? [process.env.FRONTEND_URL, 'http://localhost', 'http://localhost:3000']
+      : ['http://localhost:5173', 'http://localhost', 'http://localhost:3000'],
   credentials: true,
-  methods: ['GET', 'POST', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
 };
 
